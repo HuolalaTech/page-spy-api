bf6d42d95336600f1cdc29c57179ede9e3f40546
wangdashuaihenshuai <wangjinhai68@gmail.com>
2024-01-10 10:27:54 +0800

    fix: fix static config not exist error.

--- serve/core.go
+++ serve/core.go
@@ -9,18 +9,22 @@ import (
 
 func Run() {
 	err := container.Container().Invoke(func(e *echo.Echo, config *config.Config, staticConfig *config.StaticConfig) {
-		hash := staticConfig.GitHash
-		version := staticConfig.Version
-		if hash == "" {
-			hash = "local"
+		if staticConfig != nil {
+			hash := staticConfig.GitHash
+			version := staticConfig.Version
+			if hash == "" {
+				hash = "local"
+			}
+			if version == "" {
+				version = "local"
+			}
+			log.Infof("server info: %s@%s", version, hash)
 		}
-		if version == "" {
-			version = "local"
-		}
-		log.Infof("server info: %s@%s", version, hash)
+
 		for _, ip := range util.GetLocalIPList() {
 			log.Infof("LAN address http://%s:%s", ip, config.Port)
 		}
+
 		log.Infof("Local address http://localhost:%s", config.Port)
 		e.Logger.Fatal(e.Start(":" + config.Port))
 	})
