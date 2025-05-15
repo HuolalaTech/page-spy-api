b38e88e708e71cb080522ecac94658cdeff2b3b9
Blucas <15555602203@163.com>
2025-05-15 18:03:58 +0800

    update

--- serve/middleware/jwt.go
+++ serve/middleware/jwt.go
@@ -2,9 +2,7 @@ package middleware
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/HuolalaTech/page-spy-api/config"
@@ -21,34 +19,13 @@ type Claims struct {
 
 // InitJWTSecret 初始化JWT密钥
 func InitJWTSecret(cfg *config.Config) {
-	// 优先使用环境变量中的密钥
-	envSecret := os.Getenv("JWT_SECRET")
-	if envSecret != "" {
-		jwtSecret = []byte(envSecret)
-		return
-	}
-
-	// 其次使用配置文件中的密钥
-	if cfg.AuthConfig != nil && cfg.AuthConfig.JwtSecret != "" {
-		jwtSecret = []byte(cfg.AuthConfig.JwtSecret)
-		return
-	}
-
-	// 如果是首次启动（没有AuthConfig配置）
-	// 我们不自动创建JwtSecret，而是等到用户设置或跳过密码设置时再创建
-	if cfg.AuthConfig == nil {
+	if cfg.AuthConfig == nil || cfg.AuthConfig.JwtSecret == "" {
 		// 使用临时密钥，但不保存到配置文件
 		jwtSecret = generateRandomKey(32)
 		return
 	}
 
-	// 如果有AuthConfig但没有JwtSecret（或者JwtSecret为空）
-	// 我们只在内存中使用临时密钥，不修改配置文件
-	// 当用户设置或跳过密码设置时才会保存到配置文件
-	if cfg.AuthConfig.JwtSecret == "" {
-		jwtSecret = generateRandomKey(32)
-		return
-	}
+	jwtSecret = []byte(cfg.AuthConfig.JwtSecret)
 }
 
 // 生成随机密钥
@@ -81,21 +58,6 @@ func GenerateToken(cfg *config.Config) (string, int, error) {
 	// 确保JWT密钥已初始化
 	if len(jwtSecret) == 0 {
 		InitJWTSecret(cfg)
-
-		// 如果jwtSecret还是空的，可能是因为使用了环境变量设置密码但没有设置JWT_SECRET
-		// 此时我们需要生成一个临时密钥并保存到配置文件
-		if len(jwtSecret) == 0 && cfg.AuthConfig != nil && cfg.AuthConfig.Password != "" {
-			newSecret := generateRandomKey(32)
-			jwtSecret = newSecret
-
-			// 如果不是通过环境变量JWT_SECRET设置的，就将密钥保存到配置文件
-			if os.Getenv("JWT_SECRET") == "" {
-				// 将密钥保存到配置中
-				cfg.AuthConfig.JwtSecret = base64.StdEncoding.EncodeToString(newSecret)
-				// 保存配置到文件
-				SaveConfigToFile(cfg)
-			}
-		}
 	}
 
 	// 确定过期时间
